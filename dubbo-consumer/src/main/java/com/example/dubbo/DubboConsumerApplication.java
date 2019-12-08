568831acff3fa3b6b17f30fe76fe6334e2793545
chenjianzhao <j.623487211@163.com>
2019-12-09 00:52:41 +0800

    feat: dubbo

---
+++ dubbo-consumer/src/main/java/com/example/dubbo/DubboConsumerApplication.java
@@ -0,0 +1,29 @@
+package com.example.dubbo;
+
+import com.example.dubbo.api.DemoService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@Slf4j
+public class DubboConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboConsumerApplication.class, args);
+	}
+
+	@Reference(version = "1.0.0", url = "dubbo://127.0.0.1:12345")
+	private DemoService demoService;
+
+	@Bean
+	public ApplicationRunner runner() {
+		return args -> {
+			log.info(demoService.sayHello("mercyblitz"));
+		};
+	}
+
+}
---
+++ dubbo-consumer/src/main/java/com/example/dubbo/api/DemoService.java
@@ -0,0 +1,7 @@
+package com.example.dubbo.api;
+
+public interface DemoService {
+
+	String sayHello(String name);
+
+}
